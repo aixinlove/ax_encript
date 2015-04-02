f0cc0fe1a61e45452a8697f59fd515c4556d813b
ax <15000031393@qq.com>
2015-04-02 08:28:12 +0800

    add xor func

--- ax_encript.c
+++ ax_encript.c
@@ -8,14 +8,35 @@
 
 #include "ax_encript.h"
 
-
-
 void ax_encript_xor_enc_dec(ax_encript_block_p in, ax_encript_block_p pwd, ax_encript_block_p out){
+	*out = *in^*pwd;
+}
+void ax_encript_tea_enc(ax_encript_block_p in, ax_encript_block_p pwd, ax_encript_block_p out){
 
 }
-void ax_encript_tea_enc_dec(ax_encript_block_p in, ax_encript_block_p pwd, ax_encript_block_p out){
+void ax_encript_tea_dec(ax_encript_block_p in, ax_encript_block_p pwd, ax_encript_block_p out){
 
 }
 void ax_encript_do(ax_encript_block_p in, ax_encript_block_p pwd, ax_encript_block_p out, ax_encript_type_t type,ax_encript_func_t func){
+	switch(func){
+		case ax_encript_func_xor:{
+			ax_encript_xor_enc_dec(in,pwd,out);
+		}break;
+		case ax_encript_func_tea:{
+			if(ax_encript_type_enc==type){
+				ax_encript_tea_enc(in,pwd,out);
+			}else{
+				ax_encript_tea_dec(in,pwd,out);
+			}
+		}break;
+		case ax_encript_func_xtea:{
 
+		}break;
+		case ax_encript_func_xxtea:{
+
+		}break;
+		case ax_encript_func_rc4:{
+
+		}break;
+	}
 }
