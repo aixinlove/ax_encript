c74baf3c03d0b5d294ebbb70cc1460e7b2017e84
xyyc <5740826@qq.com>
2015-04-13 23:21:47 +0800

    add type->name function

--- ax_encript.c
+++ ax_encript.c
@@ -8,11 +8,25 @@
 
 #include "ax_encript.h"
 #include <string.h>
+#define ax_encript_min(a,b) (a)>(b)?(b):(a)
 ax_encript_func_t ax_enc_func_name_to_value(char *name){
     if (strcasecmp(name, "xor")==0) {
         return ax_encript_func_xor;
     }
     return ax_encript_func_xor;
+}
+void ax_enc_func_to_name(ax_encript_func_t func,char *name,int namelen){
+    char *funcname="";
+    switch (func) {
+        case ax_encript_func_xor:
+            funcname="xor";
+            break;
+            
+        default:
+            funcname="unknow";
+            break;
+    }
+    memcpy(name, funcname,ax_encript_min(strlen(funcname),namelen));
 }
 void ax_encript_xor_enc_dec(ax_encript_block_p in, ax_encript_block_p pwd, ax_encript_block_p out){
 	*out = *in^*pwd;
